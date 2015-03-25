7babf68665a2ae50b68d07238ba8b70261a75124
David Gorges <david.gorges@leaphub.de>
2015-03-24 20:26:41 -0700

    chore(*): initial

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # cleanslate.less
-An extreme reset style for embedabble widgets. Based on premasagar/cleanslate
+An extreme css reset mixin for embeddable widgets. Based on premasagar/cleanslate
+
+## Why?
+Sometimes, you want to reset the CSS for a specific element to avoid the parent page styles to blead into your CSS. This is often
+ the case when you write embeddable widgets or ads.
+
+## Install
+
+You can chose between _bower and npm to install:
+
+````bash
+bower install cleanslate.less
+````
+
+or
+
+````bash
+npm install cleanslate.less
+````
+
+## Usage
+Import the ``mixin.less`` and call it from your widget selector.
+
+````less
+@import "bower_components/cleanslate.less/src/mixin.less";
+      
+.my-widget {
+  .cleanslate();
+}
+````
+    
+    
---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var autoprefix = require('gulp-autoprefixer');
+var gutil = require('gulp-util');
+var minifyCSS = require('gulp-minify-css')
+var sourcemaps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
+
+gulp.task('less', function () {
+    return gulp.src('src/cleanslate.less')
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(autoprefix('last 3 version'))
+        .pipe(gulp.dest('dist'))
+        .pipe(sourcemaps.write('../dist'))
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('minify', function() {
+    return gulp.src('src/cleanslate.less')
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(autoprefix('last 3 version'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('cleanslate.min.css'))
+        .pipe(sourcemaps.write('../dist'))
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('default', ['less', 'minify']);
